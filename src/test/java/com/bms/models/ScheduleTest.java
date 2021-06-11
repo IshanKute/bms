f839b3e453b2f95ad638b44afc5d9b4859ff3d40
Ishan Kute <kute.ishan@gmail.com>
2021-06-11 20:36:07 +0530

    Add tests for Schedule

---
+++ src/test/java/com/bms/models/ScheduleTest.java
@@ -0,0 +1,20 @@
+package com.bms.models;
+
+import com.bms.services.TestData;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduleTest {
+    Schedule schedule = TestData.schedule;
+    @Test
+    void should_get_shows_for_given_movie_id() {
+        Map<Theatre, List<Show>> actualResult =  schedule.getShowsFor(1);
+        assertEquals(TestData.theatre, actualResult.keySet().toArray()[0]);
+        assertEquals(2, actualResult.get(TestData.theatre).size());
+        assertEquals(TestData.show, actualResult.get(TestData.theatre).get(0));
+    }
+}
--- src/test/java/com/bms/services/TestData.java
+++ src/test/java/com/bms/services/TestData.java
@@ -3,6 +3,7 @@ package com.bms.services;
 import com.bms.models.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class TestData {
@@ -13,7 +14,7 @@ public class TestData {
             new Movie(4, "Interstellar")
     );
 
-    private static final Show show = new Show(1, "9 AM", 100.0);
+    public static final Show show = new Show(1, "9 AM", 100.0);
 
     private static final Screen screen = new Screen("1", Arrays.asList(
             show,
@@ -35,35 +36,6 @@ public class TestData {
     public static Ticket ticket = new Ticket(movies.get(0), theatre, screen, show);
 
     public static Schedule schedule = new Schedule(
-            Arrays.asList(
-                    new Theatre("PVR", Arrays.asList(
-                            new Screen("1", Arrays.asList(
-                                    new Show(1, "9 AM", 100.0),
-                                    new Show(2, "12 PM", 100.0),
-                                    new Show(1, "3 PM", 120.0),
-                                    new Show(2, "6 PM", 120.0)
-                            )),
-                            new Screen("2", Arrays.asList(
-                                    new Show(3, "9 AM", 100.0),
-                                    new Show(4, "12 PM", 100.0),
-                                    new Show(3, "3 PM", 120.0),
-                                    new Show(4, "6 PM", 120.0)
-                            ))
-                    )),
-                    new Theatre("E-Square", Arrays.asList(
-                            new Screen("1", Arrays.asList(
-                                    new Show(3, "9 AM", 100.0),
-                                    new Show(4, "12 PM", 100.0),
-                                    new Show(3, "3 PM", 120.0),
-                                    new Show(4, "6 PM", 120.0)
-                            )),
-                            new Screen("2", Arrays.asList(
-                                    new Show(1, "9 AM", 100.0),
-                                    new Show(2, "12 PM", 100.0),
-                                    new Show(1, "3 PM", 120.0),
-                                    new Show(2, "6 PM", 120.0)
-                            ))
-                    ))
-            )
+            Collections.singletonList(theatre)
     );
 }
